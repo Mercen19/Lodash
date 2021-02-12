260b792d6930f49c9626b5a3d2f40a8f23e385ed
Mercen19 <puttersw1@gmail.com>
2021-02-12 21:42:11 +0700

    Implement _.drop()

--- _.js
+++ _.js
@@ -51,6 +51,13 @@ const _ = {
             }
         }
         return undefined;
+    },
+    drop (array, n) {
+        if (n === undefined) {
+            n = 1;
+        }
+        const droppedArray = array.slice(n);
+        return droppedArray;
     }
 };
 
